82de3f0fd6ae70b36112caa5432dbdd64e1fe6be
consultingninja <consultingninja.tech@gmail.com>
2023-05-30 14:34:35 -0500

    add user info example

--- src/routes/oauth/+server.js
+++ src/routes/oauth/+server.js
@@ -4,6 +4,14 @@ import { OAuth2Client } from 'google-auth-library';
 
 import {SECRET_CLIENT_ID,SECRET_CLIENT_SECRET} from '$env/static/private';
 
+async function getUserData(access_token) {
+
+  const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
+  console.log('response',response);
+  const data = await response.json();
+  console.log('data',data);
+}
+
 
 export const GET = async ({ url}) => {
     const redirectURL = 'http://localhost:5173/oauth';
@@ -24,6 +32,9 @@ export const GET = async ({ url}) => {
         console.info('Tokens acquired.');
         const user = oAuth2Client.credentials;
         console.log('credentials',user);
+
+        await getUserData(user.access_token);
+
 
       } catch (err) {
         console.log('Error logging in with OAuth2 user', err);
